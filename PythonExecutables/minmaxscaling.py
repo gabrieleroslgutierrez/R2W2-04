614ad8780b8d49c894293ac291d0d4dd078dad4d
jb-herrera <b2026jblherrera@pshs.edu.ph>
2025-02-18 11:59:41 +0800

    stuff

--- PythonExecutables/minmaxscaling.py
+++ PythonExecutables/minmaxscaling.py
@@ -9,7 +9,6 @@ def search_min(data_list):
     for i in range(len(data_list)):
         val = data_list.iloc[i]
         if val < min: min = val
-    print(min)
     return min
 
 #func to search max value
@@ -18,7 +17,6 @@ def search_max(data_list):
     for i in range(len(data_list)):
         val = data_list.iloc[i]
         if val > max: max = val
-    print(max)
     return max
 
 #func to standardize given min max values (minmax scaling)
@@ -32,11 +30,8 @@ def standardize(data_list, min, max):
 input_file = input("Input file location: ")
 df = csv.read_csv_file(input_file)
 
-output_file = input("Save CSV File to (include filename in path <path>\\<filename>.csv): ")
-
 #define empty df
 std_df = pd.DataFrame()
-print(std_df)
 
 '''
 go through each column to standardize them
@@ -56,7 +51,7 @@ except Exception as e:
 
 
 try:
-    print(std_df)
+    output_file = input("Save CSV File to (include filename in path <path>\\<filename>.csv): ")
     std_df.to_csv(output_file)
 except Exception as e:
     print(f"Failed to save at the specified location: {e}")
