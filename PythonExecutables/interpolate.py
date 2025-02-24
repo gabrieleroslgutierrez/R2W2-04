afae20907d116869adb9b31bf23235a99dae999f
jb-herrera <b2026jblherrera@pshs.edu.ph>
2025-02-24 21:24:21 +0800

    did some stuff

---
+++ PythonExecutables/interpolate.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import csvreadwritetemplate as csv
+
+def date_average():
+    return
+
+def line_interpolate():
+    return
+
+###
+input_file = input("Input file location: ")
+df = csv.read_csv_file(input_file)
