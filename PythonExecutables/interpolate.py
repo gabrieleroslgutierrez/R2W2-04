f9009a2dd3a6a79f3ef739ede3eaab6c0382e657
jb-herrera <b2026jblherrera@pshs.edu.ph>
2025-02-26 12:05:35 +0800

    magic

--- PythonExecutables/interpolate.py
+++ PythonExecutables/interpolate.py
@@ -7,8 +7,22 @@ def split_col(data,cut_index):
     return df1, df2
 
 def date_average(dates,date_x,col):
-    
-    return
+    val = 0
+    count = 0  
+    for i in range(len(dates)):
+        if date_x["MONTH"] == dates.iloc[i,1] and date_x["DAY"] == dates.iloc[i,2]:
+            if date_x["YEAR"] != dates.iloc[i,0] and col[i] >= 0:
+                val = val + col[i]
+                print(col[i])   
+                count = count + 1
+    if count == 0: count = 1
+    val = val/count
+    print("---")
+    print(val)
+    print("---")
+    return val
+
+
 
 def line_interpolate(a_index,b_index,a_val,b_val,gap):
     return
@@ -23,17 +37,22 @@ data_average = pd.DataFrame()
 line_interpolation = pd.DataFrame()
 
 print(data)
+print(dates)
 
 ave = pd.DataFrame()
-line = pd.DataFrame()
 
 for col in data:
     dat = data[col]
-    temp_ave = pd.DataFrame(columns=[f"{col}"])
-    temp_line = pd.DataFrame(columns=[f"{col}"])
+    print(dat)
     for i in range(len(dat)):
         val = dat.iloc[i]
+        print(val)
         if val < 0:
-            dv = data_average(dates,date.iloc[i],dat)
-            temp_ave.loc[len(temp_ave)] = dv
-    ave = pd.concat([ave, temp_ave], axis=1, join="outer")
+            print("yes")
+            dv = date_average(dates,dates.iloc[i],dat)
+            dat[i] = dv
+    ave = pd.concat([ave, dat], axis=1, ignore_index=True)
+
+print(ave)
+ave.to_csv("testing.csv", index=False)
+    
